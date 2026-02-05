b797ac92fa83c61d863d45dfa911cb4e41fe78ea
ニコライ <nikaskovmk601@gmail.com>
2026-02-05 01:40:52 +0100

    mooooooooooooore dagestani shittt yeaaaah

--- src/public/script/app.js
+++ src/public/script/app.js
@@ -2,25 +2,38 @@
 (function () {
     const sidebar = document.querySelector('.app-sidebar');
     const toggle = document.querySelector('[data-sidebar-resizer]');
+    const themeToggle = document.querySelector('[data-theme-switcher]');
+    const doc = document.documentElement;
 
 
     if (!sidebar || !toggle) return;
+    if (!themeToggle) return;
 
 
-    const STORAGE_KEY = 'sidebar:collapsed';
-
+    const SIDEBAR_STORAGE_KEY = 'sidebar:collapsed';
+    const THEME_STORAGE_KEY = 'themeSwitched';
 
     // Initial State
-    if (localStorage.getItem(STORAGE_KEY) === 'true') {
-        sidebar.classList.add('is-collapsed');
+    if (localStorage.getItem(SIDEBAR_STORAGE_KEY) === 'true') {
+        sidebar.classList.add('--resized');
+    }
+    if (localStorage.getItem(THEME_STORAGE_KEY) === 'night') {
+        doc.classList.add('--dark-mode');
     }
 
 
     toggle.addEventListener('click', function () {
-        sidebar.classList.toggle('is-collapsed');
+        sidebar.classList.toggle('--resized');
         localStorage.setItem(
-            STORAGE_KEY,
-            sidebar.classList.contains('is-collapsed')
+            SIDEBAR_STORAGE_KEY,
+            sidebar.classList.contains('--resized')
+        );
+    });
+    themeToggle.addEventListener('click', function () {
+        doc.classList.toggle('--dark-mode');
+        localStorage.setItem(
+            THEME_STORAGE_KEY,
+            doc.classList.contains('--resized')
         );
     });
 })();
